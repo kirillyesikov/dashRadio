7b8f896ce877170ce08f735cb68cbdb7a6f42436
ViceBooster <nicholaslcorrigan@gmail.com>
2025-01-09 10:02:05 -0500

    Add support for env file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
